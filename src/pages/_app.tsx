61150d19a4d64c7ff18a2385c43fc29476575296
azwarhhalimu <azwarhalimu@gmail.com>
2023-10-11 16:22:09 +0800

    mantap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar';
 export default function App({ Component, pageProps }: AppProps) {
   return <MainLayoutx>
-    <NextNProgress color="#F97E00" startPosition={0.3} stopDelayMs={200} height={6} showOnShallow={true} />
+    <NextNProgress color="#FF0009" startPosition={0.3} stopDelayMs={200} height={6} showOnShallow={false} />
     <Component {...pageProps} />
   </MainLayoutx>
 }
