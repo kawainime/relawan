fcc77e9468bf9333520ab27ac673b403deedf191
azwarhhalimu <azwarhalimu@gmail.com>
2023-10-12 17:53:51 +0800

    data berhasil di tambahkan

---
+++ src/Componen/EditPendukung.tsx
@@ -0,0 +1,154 @@
+import baseUrl from "@/config";
+import { inRelawan } from "@/pages/data-calon-pendukung.html";
+import axios, { AxiosResponse } from "axios";
+import { ValueOf } from "next/dist/shared/lib/constants";
+import queryString from "query-string";
+import React, { useEffect, useState } from "react";
+
+interface inDataKelurahan {
+    id_kelurahan: string,
+    kelurahan: string
+}
+interface inDataTps {
+    id_tps: string, tps: string,
+}
+interface inDataProps {
+    id_pendukung: string,
+    nama: string, nik: string, jenis_kelamin: string,
+    usia: string, rt_rw: string, kelurahan: string,
+    id_relawan: string, id_tps: string, tps: string, id_kelurahan: string,
+    batal: any,
+    reload: any
+
+    data_kelurahan: inDataKelurahan[],
+    data_relawan: inRelawan[],
+
+}
+const EditPendukung: React.FC<inDataProps> = (props) => {
+    const [nama, setNama] = useState<string>(props.nama);
+    const [nik, setNik] = useState<string>(props.nik);
+    const [id_tps, setId_tps] = useState<string>(props.id_tps);
+    const [jenis_kelamin, setJenis_kelamin] = useState<string>(props.jenis_kelamin);
+    const [usia, setUsia] = useState<string>(props.usia);
+    const [rt_rw, setRt_rw] = useState<string>(props.rt_rw);
+    const [kelurahan, setKelurahan] = useState<string>(props.id_kelurahan);
+    const [id_relawan, setId_relawan] = useState<string>(props.id_relawan);
+    const [tps, setTps] = useState<string>(props.tps);
+    const [dataKelurahan, setDataKelurahan] = useState<inDataKelurahan[]>(props.data_kelurahan);
+    const [isProses, setIsProses] = useState<boolean>(false);
+
+    const [dataTps, setDataTps] = useState<inDataTps[]>([]);
+    useEffect(() => {
+        _getTps();
+    }, [])
+    const _getTps = () => {
+        axios.get(baseUrl("list-tps/" + props.id_kelurahan))
+            .then((respon) => {
+                setDataTps(respon.data);
+            })
+    }
+
+
+    const _update = () => {
+
+        setIsProses(true);
+        axios.put(baseUrl('update-pendukung/' + props.id_pendukung), queryString.stringify({
+            nama: nama, nik: nik, jenis_kelamin: jenis_kelamin, usia: usia,
+            rt_rw: rt_rw, kelurahan: kelurahan,
+            id_relawan: id_relawan, id_tps: id_tps,
+        })).then((respon: AxiosResponse<any, any>) => {
+            if (respon.data.status == "data_terupdate") {
+                props.batal();
+                props.reload();
+                alert("UPDATE SUKSES");
+            }
+        });
+    }
+    return (<>
+        <tr>
+            <td>**</td>
+            <td>
+                <input onChange={(e) => {
+                    setNik(e.target.value);
+                }} value={nik} type="text" autoFocus placeholder="NIK...." className="form-control" />
+            </td>
+            <td>
+                <input
+                    onChange={(e) => {
+                        setNama(e.target.value);
+                    }}
+                    type="text" value={nama} placeholder="Masukkan nama...." className="form-control" />
+            </td>
+            <td>
+                <select
+                    onChange={(e) => {
+                        setJenis_kelamin(e.target.value);
+                    }}
+                    value={jenis_kelamin} className="form-control">
+                    <option value={""}>Pilih Jenis Kelmain</option>
+                    <option value={"Laki-laki"}>Laki-laki</option>
+                    <option value={"Perempuan"}>Perempuan</option>
+                </select>
+            </td>
+            <td>
+                <input
+                    onChange={(e) => {
+                        setUsia(e.target.value);
+                    }}
+                    value={usia} type="number" placeholder="Masukkan usia" className="form-control" />
+            </td>
+            <td>
+                <select
+                    onChange={(e) => {
+                        setKelurahan(e.target.value);
+                    }}
+                    value={kelurahan} defaultValue={kelurahan} className="form-control">
+                    <option value={""}>Pilih Kelurahan</option>
+                    {dataKelurahan.map((list, index) => (
+                        <option key={index} value={list.id_kelurahan}>{list.kelurahan}</option>
+                    ))}
+                </select>
+            </td>
+            <td>
+                <input type="text" value={rt_rw} className="form-control" placeholder="04/05" />
+            </td>
+
+            <td>
+
+                <select
+                    onChange={(e) => {
+                        setTps(e.target.value);
+                    }}
+                    value={id_tps} className="form-control">
+                    <option value={""}>TPS</option>
+                    {dataTps.map((list, index) => (
+                        <option key={`dfa${index}`} value={list.id_tps}>{list.tps}</option>
+                    ))}
+
+                </select>
+            </td>
+            <td>
+                <select value={id_relawan} className="form-control">
+                    <option value={""}>RELAWAN</option>
+                    {props.data_relawan.map((list, index) => (
+                        <option key={`3r${index}`} value={list.id_relawan}>{list.nama}</option>
+                    ))}
+
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td colSpan={9} style={{ textAlign: "center" }}>
+                {isProses ? "Mengupdate.... " : <button className="btn btn-success" onClick={() => {
+                    _update()
+                }}>Update Data</button>}
+                {" "}
+                <button className="btn btn-danger" onClick={() => {
+                    props.batal()
+                }}>Batal</button>
+            </td>
+        </tr>
+    </>);
+}
+
+export default EditPendukung;
--- src/pages/data-calon-pendukung.html.tsx
+++ src/pages/data-calon-pendukung.html.tsx
@@ -1,4 +1,5 @@
 import DataKosong from "@/Componen/DataKosong";
+import EditPendukung from "@/Componen/EditPendukung";
 import LoadingTable from "@/Componen/LoadingTable";
 import baseUrl from "@/config";
 import axios, { AxiosResponse } from "axios";
@@ -15,15 +16,34 @@ interface Data {
     usia: string,
     rt_rw: string,
     tps: string,
+    id_tps: string,
+    id_kelurahan: string,
     kelurahan: string,
     nama_relawan: string,
     id_relawan: string,
 }
+export interface inRelawan {
+    id_relawan: string,
+    nama: string,
+}
+interface inKelurahan {
+    id_kelurahan: string, kelurahan: string,
+}
 const Pendukung: React.FC = () => {
+
 
     const [dataKosong, setDataKosong] = useState<boolean>(false);
     const [data, setData] = useState<Data[]>();
     const [loading, setLoading] = useState<boolean>(false);
+    const [edit, setEdit] = useState<string>();
+    const [dataKelurahan, setDataKelurahan] = useState<inKelurahan[]>([]);
+    const [reload, setReload] = useState<number>(0);
+
+    const [dataRelawan, setDataRelawan] = useState<inRelawan[]>([]);
+
+    const _batal = () => {
+        setEdit("");
+    }
     const _getdata = () => {
         setLoading(true);
         axios.get(baseUrl("pendukung"))
@@ -37,8 +57,41 @@ const Pendukung: React.FC = () => {
 
             })
     }
+    const _hapus = (id_pendukung: string) => {
+        const confim = window.confirm("Apakah anda ingin hapus data ini?");
+        if (confim) {
+            axios.delete(baseUrl('delete-pendukung/' + id_pendukung))
+                .then((respon: AxiosResponse<any, any>) => {
+                    if (respon.data.status == "data_deleted") {
+                        alert("Data berhasil di hapus");
+                        setReload(reload + 5);
+                    }
+                })
+        }
+    }
+    const _getKelurahan = () => {
+        axios.get(baseUrl("get-kelurahan"))
+            .then((respon: AxiosResponse<any, any>) => {
+                setDataKelurahan(respon.data);
+            })
+    }
+    const _getRelawan = () => {
+        axios.get(baseUrl('get-relawan-all'))
+            .then((respon: AxiosResponse<any, any>) => {
+                setDataRelawan(respon.data);
+            })
+    }
+
+    const _reload = () => {
+        setReload(reload + 1);
+    }
     useEffect(() => {
         _getdata();
+    }, [reload])
+    useEffect(() => {
+
+        _getKelurahan();
+        _getRelawan();
     }, [])
 
     return (<>
@@ -73,51 +126,66 @@ const Pendukung: React.FC = () => {
                 </div>
                 <div className="card-body">
 
+                    <div className="table-responsive">
+                        <table className="table table-bordered">
+                            <thead style={{ background: "#1783FF", color: "white", fontWeight: "bold" }}>
+                                <tr>
+                                    <td>No</td>
+                                    <td>Nik</td>
+                                    <td>Nama</td>
+                                    <td>Jenis Kelamin</td>
+                                    <td width={"130px"}>Usia</td>
+                                    <td>Kelurahan</td>
+                                    <td width={"130px"}>RT/RW</td>
+                                    <td>TPS</td>
+                                    <td>Relawan</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {loading ? <LoadingTable baris={8} kolom={9} /> :
 
-                    <table className="table table-bordered">
-                        <thead style={{ background: "#1783FF", color: "white", fontWeight: "bold" }}>
-                            <tr>
-                                <td>No</td>
-                                <td>Nik</td>
-                                <td>Nama</td>
-                                <td>Jenis Kelamin</td>
-                                <td>Usia</td>
-                                <td>Kelurahan</td>
-                                <td>RT/RW</td>
-                                <td>TPS</td>
-                                <td>Relawan</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {loading ? <LoadingTable baris={8} kolom={9} /> : data?.map((list, index) => (
-                                <>
-                                    <tr key={`adfad${index}`}>
-                                        <td>{index + 1}.</td>
-                                        <td>{list.nik}</td>
-                                        <td>{list.nama}</td>
-                                        <td>{list.jenis_kelamin}</td>
-                                        <td>{list.usia}</td>
-                                        <td>{list.kelurahan}</td>
-                                        <td>{list.rt_rw}</td>
-                                        <td>{list.tps}</td>
-                                        <td>{list.nama_relawan}</td>
-                                    </tr>
-                                    <tr>
-                                        <td colSpan={9} style={{ textAlign: "center" }}>
-                                            Opsi Menu :
-                                            <button className="btn btn-danger"><i className="fa fa-trash" /> Hapus</button>
-                                            {" "}
-                                            <button className="btn btn-warning"><i className="fa fa-edit" /> Edit</button>
-                                        </td>
-                                    </tr>
-                                </>
+                                    data?.map((list, index) => (
+                                        edit == list.id_pendukung ? <EditPendukung
+                                            key={index}
+                                            id_pendukung={list.id_pendukung}
+                                            reload={_reload}
+                                            batal={_batal}
+                                            id_tps={list.id_tps}
+                                            data_kelurahan={dataKelurahan}
+                                            nama={list.nama}
+                                            nik={list.nik} jenis_kelamin={list.jenis_kelamin}
+                                            usia={list.usia} rt_rw={list.rt_rw} id_kelurahan={list.id_kelurahan}
+                                            id_relawan={list.id_relawan} tps={list.tps} kelurahan={list.kelurahan}
+                                            data_relawan={dataRelawan} /> : <>
+                                            <tr key={`adfad${index}`}>
+                                                <td>{index + 1}.</td>
+                                                <td>{list.nik}</td>
+                                                <td>{list.nama}</td>
+                                                <td>{list.jenis_kelamin}</td>
+                                                <td>{list.usia}</td>
+                                                <td>{list.kelurahan}</td>
+                                                <td>{list.rt_rw}</td>
+                                                <td>{list.tps}</td>
+                                                <td>{list.nama_relawan}</td>
+                                            </tr>
+                                            <tr>
+                                                <td colSpan={9} style={{ textAlign: "center" }}>
+                                                    Opsi Menu :
+                                                    <button onClick={() => {
+                                                        _hapus(list.id_pendukung)
+                                                    }} className="btn btn-danger"><i className="fa fa-trash" /> Hapus</button>
+                                                    {" "}
+                                                    <button onClick={() => {
+                                                        setEdit(list.id_pendukung);
+                                                    }} className="btn btn-warning"><i className="fa fa-edit" /> Edit</button>
+                                                </td>
+                                            </tr>
+                                        </>
+                                    ))}
+                            </tbody>
+                        </table>
+                    </div>
 
-                            ))}
-
-
-
-                        </tbody>
-                    </table>
                     {dataKosong && <DataKosong />}
                 </div>
             </div>
