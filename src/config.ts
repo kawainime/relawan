5c35b14aa4756b122071da57b8f5e34a583c27d5
azwarhhalimu <azwarhalimu@gmail.com>
2023-10-19 18:32:34 +0800

    fafdf

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // let host: string = "http://localhost:2001";
-let host: string = "https://5shaf.azwarbuton.biz.id";
+let host: string = "https://new-5shaf.azwarbuton.biz.id";
 const  baseUrl = (url: string): string => { 
    let result:string= host +'/'+ url;
     return result;
