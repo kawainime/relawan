4fe72a14466092101c7104519af2efe69082df8d
azwarhhalimu <azwarhalimu@gmail.com>
2023-10-28 22:56:25 +0800

    fadfdf

--- src/Componen/report/SemuaRelawan.tsx
+++ src/Componen/report/SemuaRelawan.tsx
@@ -98,7 +98,7 @@ const Semua_relawan: React.FC = () => {
                 const blob = new Blob([respon.data], { type: type })
                 const link = document.createElement('a')
                 link.href = window.URL.createObjectURL(blob)
-                link.download = 'file.xlsx'
+                link.download = 'file.xls'
                 link.click()
                 setDownload(false);
             })
