09101f23f371bc8a86116118e26c6a62e48303b6
KawaiNime Pro <147604824+kawainime@users.noreply.github.com>
2023-11-20 01:47:46 +0700

    gtw

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,28 +6,17 @@ import "@/assets/loading-spinner.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar';
-import LoginComponen from '@/layouts/LoginComponen';
 import dynamic from 'next/dynamic';
 import { MyProvider } from '@/interface/myContext';
 
 
 
 const Admin = dynamic(() => import('@/layouts/Mainlayout'), { ssr: true })
-const Login = dynamic(() => import('@/layouts/LoginComponen'), { ssr: true })
 export default function App({ Component, pageProps }: AppProps) {
 
 
 
-  if (typeof window !== "undefined") {
-    if (window.location.pathname.split('/')[1] == "login.html") {
-      console.log('c');
-      return <Login>
-        <NextNProgress color="#FF0009" startPosition={0.3} stopDelayMs={200} height={6} showOnShallow={false} />
-        <Component {...pageProps} />
-      </Login>
 
-
-    }
     else {
       return <MyProvider>
         <Admin>
